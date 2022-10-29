0e955ff781b99fa84b964efa6bc208cef4334fdd
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2022-10-29 14:29:54 +0200

    Fix license header
    Signed-off-by: Seppe Volkaerts <seppevolkaerts@hotmail.com>

--- src/test/java/org/lanternpowered/lmbda/Java15Test.java
+++ src/test/java/org/lanternpowered/lmbda/Java15Test.java
@@ -1,3 +1,12 @@
+/*
+ * Lmbda
+ *
+ * Copyright (c) LanternPowered <https://www.lanternpowered.org>
+ * Copyright (c) contributors
+ *
+ * This work is licensed under the terms of the MIT License (MIT). For
+ * a copy, see 'LICENSE.txt' or <https://opensource.org/licenses/MIT>.
+ */
 package org.lanternpowered.lmbda;
 
 import org.junit.jupiter.api.Test;
